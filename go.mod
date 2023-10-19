8c710749e77e15a1722a696671e5e3abf2471896
fujie feng <fengfujie01@longfor.com>
2023-10-19 12:11:52 +0800

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module go_code
+`module go_code
 
 go 1.21.1
--- src/project_01/main/helo.go
+++ src/project_01/main/helo.go
@@ -2,7 +2,36 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("heol word!")
-	fmt.Println("test")
+type ListNode struct {
+    Val  int
+    Next *ListNode
+}
+
+// 反转链表函数
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode
+    curr := head
+    for curr != nil {
+        next := curr.Next
+        curr.Next = prev
+        prev = curr
+        curr = next
+    }
+    return prev
+}
+
+func main() {
+    // 创建链表
+    head := &ListNode{Val: 1, Next: nil}
+    node1 := &ListNode{Val: 2, Next: nil}
+    node2 := &ListNode{Val: 3, Next: nil}
+    head.Next = node1
+    node1.Next = node2
+
+    // 反转链表并打印结果
+    newHead := reverseList(head)
+    for newHead != nil {
+        fmt.Println(newHead.Val)
+        newHead = newHead.Next
+    }
 }
